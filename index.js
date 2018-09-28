0b48fd98d81df23b6c1eb17a688a091ec8aefef8
Chris Cardle <cdcardle@gmail.com>
2018-09-28 13:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(city) {
-  city === "NYC" ? "Ok, sounds good." : "No go.";
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
 }
